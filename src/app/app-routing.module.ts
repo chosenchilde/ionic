031d3df28dfe911445efb9903cc5550cd74f1318
Chosen Child <edencyberspace@gmail.com>
2023-06-28 19:41:40 -0300

    commitando coisas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { AboutComponent } from './pages/about/about.component';
+import { E404Component } from './pages/e404/e404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  // Rota padrão → Página inicial → 'home'.
+  // Deve ser SEMPRE a primeira rota.
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+
+  // Página 'home'.
+  {
+    path: 'home',
+    title: 'Página Inicial',
+    component: HomeComponent
+  },
+
+  {
+    path: 'contacts',
+    title: 'Faça contato',
+    component: ContactsComponent
+  },
+
+  {
+    path: 'about',
+    title: 'Sobre',
+    component: AboutComponent
+  },
+
+  {
+    path: '404',
+    title: 'Erro 404',
+    component: E404Component
+  },
+
+  // Se uma rota não existe, vai para a página 404.
+  // deve ser SEMPRE a última rota.
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { E404Component } from './pages/e404/e404.component';
+import { ThemeComponent } from './theme/theme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ThemeComponent,
+    AboutComponent,
+    ContactsComponent,
+    E404Component
   ],
   imports: [
     BrowserModule,
