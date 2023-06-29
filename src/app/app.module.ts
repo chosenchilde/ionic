8398d7b3e7348c30cd796efb21ae9652ec46f61b
Chosen Child <edencyberspace@gmail.com>
2023-06-28 21:40:19 -0300

    commitando mais coisas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { ThemeComponent } from './theme/theme.component';
     E404Component
   ],
   imports: [
+    HttpClientModule, 
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  receita: any;
+
+  constructor(
+    private http: HttpClient) { this.getAllRecipes() }
+
+  ngOnInit() { }
+
+  getAllRecipes() {
+    this.http.get('http://localhost:8080/receita')
+      .subscribe((response) => { 
+        console.log(response);
+        this.receita = response;
+      },
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
 
 }
